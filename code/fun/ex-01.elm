72d9941c0b90017e3a8f98064418e52d5eb673dd
aiires <31633775+aiires@users.noreply.github.com>
2018-03-05 14:28:22 +0000

    Update ex-01.elm

--- code/fun/ex-01.elm
+++ code/fun/ex-01.elm
@@ -17,10 +17,15 @@ media (_,n1,n2) = (n1+n2)/2     -- o nome é ignorado
 nome: Aluno -> String
 nome (nm,_,_) = nm              -- as notas são ignoradas
 
+notasaluno: Aluno -> List Float
+notasaluno (_,n1,n2) = [n1,n2]
+
 -- Por fim, considere as assinaturas para as funções map, filter, e fold a seguir:
 
 --List.map: (a->b) -> (List a) -> (List b)
   -- mapeia uma lista de a's para uma lista de b's com uma função de a para b
+ 
+ 
 
 --List.filter: (a->Bool) -> (List a) -> (List a)
   -- filtra uma lista de a's para uma nova lista de a's com uma função de a para Bool
@@ -36,33 +41,35 @@ nome (nm,_,_) = nm              -- as notas são ignoradas
 -- Usando as definições acima, forneça a implementação para os três trechos marcados com <...>:
 
 turma: Turma
-turma = [ ("Joao",7,4), ("Maria",10,8), ... ]       -- 50 alunos
+turma = [ ("Joao",7,4), ("Maria",10,8)]       -- 50 alunos
 
 -- a) LISTA COM AS MÉDIAS DOS ALUNOS DE "turma" ([5.5, 9, ...])
 medias: List Float
-medias = ...
+medias = List.map media turma
 
 -- b) LISTA COM OS NOMES DOS ALUNOS DE "turma" APROVADOS (["Maria", ...])
 aprovados: List String
-aprovados = ...
+aprovados = List.map nome (List.filter (\a -> (media a) >= 7) turma)
 
 -- c) MÉDIA FINAL DOS ALUNOS DE "turma" (média de todas as médias)
 total: Float
-total = ...
+total = List.foldl (\a b -> (media a) + b ) 0 turma
 
 -- d) LISTA DE ALUNOS QUE GABARITARAM A P1 ([("Maria",10,8), ...])
 turma_dez_p1: Turma
-turma_dez_p1 = ...
+turma_dez_p1 = List.filter (\(_,n1,_) -> n1 ==10) turma
 
 -- e) LISTA COM OS NOMES E MEDIAS DOS ALUNOS APROVADOS ([("Maria",9), ...])
 aprovados2: List (String,Float)
-aprovados2 = ...
+aprovados2 = List.map  (\a -> (nome a,media a)) (List.filter (\a -> (media a) >= 7) turma)
 
 -- f) LISTA COM TODAS AS NOTAS DE TODAS AS PROVAS ([7,4,10,8,...])
 notas: List Float
-notas = ...
+notas = List.foldl (\a b -> List.append b (notasaluno a)) [] turma 
 
 -- É permitido usar funções auxiliares, mas não é necessário.
 -- (As soluções são pequenas.)
 
-main = text (toString turma)
+
+
+main = text (toString notas)
